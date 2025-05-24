843ba0099237487d35e3b93a14b6cfe5fc497103
plwtx <h.lenny933@gmail.com>
2025-05-25 00:10:43 +0200

    0.01 | Base

--- src/pages/main_page.jsx
+++ src/pages/main_page.jsx
@@ -1,11 +1,12 @@
 import Home from "../components/home.jsx";
 import Empty from "../components/empty.jsx";
+import ObjectViewer from "../components/3d-object-viewer.jsx";
 
 function MainPage() {
   return (
     <>
       <div className="relative overflow-x-clip">
-        <Home />
+        <ObjectViewer />
       </div>
     </>
   );
